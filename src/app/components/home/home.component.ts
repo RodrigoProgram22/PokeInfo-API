80006231766369afaca68e6232b1629f31693c40
Rodrigo Ezequiel Romano <rodrigoprograma22@gmail.com>
2024-03-04 22:47:52 -0300

    Se maqueto y programo el home de la app.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
+import { PokemonService } from 'src/app/service/pokemon-api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  pokemonList: any[] = [];
+  pokemonUno: any = {};
+  pokemonDos: any = {};
 
+  constructor(private pokemonService: PokemonService) {}
+
+  ngOnInit(): void {
+    this.getAllPokemon();
+  }
+  refrestar(){
+    this.getAllPokemon();
+  }
+
+  getAllPokemon() {
+    this.pokemonService.getAllPokemon().subscribe(
+      (data: any) => {
+        this.pokemonList = data.results;
+        this.getPokemonDetails();
+        this.pokemonUno = this.pokemonList[this.generarNumeroAleatorio()];
+        this.pokemonDos = this.pokemonList[this.generarNumeroAleatorio()];
+      },
+      (error) => {
+        console.error('Error al obtener los datos:', error);
+      }
+    );
+  }
+
+  getPokemonDetails() {
+    for (const pokemon of this.pokemonList) {
+      this.pokemonService
+        .getPokemonDetails(pokemon.url)
+        .subscribe((details: any) => {
+          pokemon.details = details;
+          if (pokemon.name) {
+            pokemon.name =
+              pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+          }
+        });
+    }
+  }
+  generarNumeroAleatorio(): number {
+    // Genera un número aleatorio entre 1 y 150
+    return Math.floor(Math.random() * 150) + 1;
+  }
 }
